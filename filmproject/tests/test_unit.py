7d75215b98bd3abd8eac878b9636c39c45f79653
Travis Ditmanson <tdtimans@uccs.edu>
2024-10-20 08:29:39 -0600

    automated tests notlogged in

--- filmproject/tests/test_unit.py
+++ filmproject/tests/test_unit.py
@@ -1,8 +1,9 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from .models import *
+from ..models import *
 from django.utils import timezone
 from django.urls import reverse, resolve
+
 
 # 1st test to make sure our testing environment is working
 class SimpleTest(TestCase):
