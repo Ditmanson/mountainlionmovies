0cea28054187c5cfaf9e2419100e62c3b8ef1d1f
Kroleski <rkroleski@comcast.net>
2024-10-20 18:50:48 -0600

    movie rating comparisons

--- filmproject/urls.py
+++ filmproject/urls.py
@@ -48,7 +48,6 @@ urlpatterns = [
     # Password reset URLs
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='filmproject/password_reset.html'), name='password_reset'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='filmproject/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='filmproject/password_reset_done.html'), name='password_reset_done'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='filmproject/password_reset_complete.html'), name='password_reset_complete'),
 
     # Movie search and popular movies
