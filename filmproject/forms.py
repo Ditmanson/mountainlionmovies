eba989d00ed6cd2ebb61cffd37d8ea2123de1bf7
nengler <nengler@uccs.edu>
2024-10-22 16:14:29 -0600

    Profile Picture file entered with User Registration

--- filmproject/forms.py
+++ filmproject/forms.py
@@ -5,18 +5,20 @@ from .models import Viewer
 
 class ViewerRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    profile_picture = forms.ImageField(required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'profile_picture']
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
+        user.profile_picture = self.cleaned_data['profile_picture']
         if commit:
             user.save()
             # Create the Viewer instance associated with the User
-            Viewer.objects.create(user=user, name=user.username, email=user.email)
+            Viewer.objects.create(user=user, name=user.username, email=user.email, profile_picture=user.profile_picture)
         return user
 
 
