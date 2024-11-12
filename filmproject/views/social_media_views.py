d9b9050f885866978994f7ed34b49475683d2889
MayberryKory <102997911+MayberryKory@users.noreply.github.com>
2024-11-12 09:00:16 -0700

    fix merge conflict pls?

--- filmproject/views/social_media_views.py
+++ filmproject/views/social_media_views.py
@@ -12,15 +12,7 @@ from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 >>>>>>> b1f36e4 (like/comment notifications complete + bugfixes)
 from django.core.paginator import Paginator
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, redirect, render
-from ..models import ( FeedEntry, Like, Comment, Notification)
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect, render
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_POST
-from django.http import JsonResponse
-from django.core.paginator import Paginator
+
 
 
 
