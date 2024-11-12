d3dc494cdf92c5a96795963936273d8447871d1e
MayberryKory <102997911+MayberryKory@users.noreply.github.com>
2024-11-12 08:47:08 -0700

    like/comment notifications complete + bugfixes

--- filmproject/views/social_media_views.py
+++ filmproject/views/social_media_views.py
@@ -8,8 +8,9 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.http import JsonResponse
-from django.core.paginator import Paginator
-from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from ..models import ( FeedEntry, Like, Comment, Notification)
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from ..models import ( FeedEntry, Like, Comment, Notification)
 from django.contrib.auth.decorators import login_required
